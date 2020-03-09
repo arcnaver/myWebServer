f13b4eca2fc2e9216fc5f812e85dd04ad2a2537a
arcnaver <arcnaver@cableone.net>
2020-03-09 10:26:32 -0600

    updating packages

---
+++ server.js
@@ -0,0 +1,10 @@
+/*
+    myWebServer
+    A simple webserver for Raspberry Pi3
+    @author: Adam Tipton
+    @version: 1.0
+    @date: 3/9/2020
+*/
+
+//create a port to use locally(5000) and remotely
+const PORT = process.env.PORT || 5000;
