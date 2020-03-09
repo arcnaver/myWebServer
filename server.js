9d5a61c8df9b287a33fda190be57b58908b72a8a
arcnaver <arcnaver@cableone.net>
2020-03-09 10:39:25 -0600

    updating server.js with boilerplate info

--- server.js
+++ server.js
@@ -8,3 +8,14 @@
 
 //create a port to use locally(5000) and remotely
 const PORT = process.env.PORT || 5000;
+//get express
+const express = require("express");
+const app = express();
+//require path
+const path = require('path');
+//we want to use static files in the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+//Tell console where we are listening
+app.listen(PORT, ()=> console.log('myWebServer is Listening on ${PORT]'));
